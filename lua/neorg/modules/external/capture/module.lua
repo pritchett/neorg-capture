6f47f426b54c088a1b17a270eb928d6b7593ed40
Brian Pritchett <brian@brianpritchett.com>
2023-10-26 19:39:20 -0500

    Remove `type` of template
    Originally I had intended to follow org-capture's "type" feature, but
    I'm just going to have that handled in the template instead.

--- lua/neorg/modules/external/capture/module.lua
+++ lua/neorg/modules/external/capture/module.lua
@@ -80,7 +80,7 @@ module.private = {
     local data = {}
 
     local build_data_entry = function(item)
-      return { template = item.name, file = item.file, type = item.type, headline = item.headline, path = item.path, query = item.query }
+      return { template = item.name, file = item.file, headline = item.headline, path = item.path, query = item.query }
     end
 
     local item_is_enabled = function(item)
@@ -88,12 +88,10 @@ module.private = {
     end
 
     for _, item in ipairs(conf.templates) do
-      if item.type then
-        if item_is_enabled(item) then
-          if item.name and item.name ~= "" then
-            table.insert(items, item.description)
-            table.insert(data, build_data_entry(item))
-          end
+      if item_is_enabled(item) then
+        if item.name and item.name ~= "" then
+          table.insert(items, item.description)
+          table.insert(data, build_data_entry(item))
         end
       end
     end
@@ -142,81 +140,79 @@ module.private = {
     end
 
     local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-    if data.type == "entry" then
 
-      local ts = module.required['core.integrations.treesitter']
+    local ts = module.required['core.integrations.treesitter']
 
-      local end_line = function(node)
-        local child_count = node:child_count()
-        if child_count > 0 then
-          local child = node:child(child_count - 1)
-          return child:end_()
-        else
-          return node:end_()
-        end
-      end
-
-      local set_lines_and_write = function(end_linenr)
-        vim.api.nvim_buf_set_lines(target_bufnr, end_linenr, end_linenr, false, lines)
-        vim.api.nvim_buf_call(target_bufnr, function()
-
-          --Works if the buffer was already open
-          vim.cmd.norm( { range = { end_linenr + 1, end_linenr + #lines }, args = { "==" } })
-
-          vim.cmd.write({ args = { path }, bang = true })
-        end)
-      end
-
-      local cb = function(query, id, node, _)
-        if(query.captures[id] ~= "org-capture-target") then
-          return false
-        end
-        local end_linenr = end_line(node)
-        set_lines_and_write(end_linenr)
-        return true -- Returning true makes `ts.execute_query` stop iterating over captures
-      end
-
-      local get_headingnr = function(i)
-        if i > 6 then
-          return 6
-        else
-          return i
-        end
-      end
-
-      local build_query = function(headline_path)
-        local query = "("
-        local end_parens = ""
-        for i, headline in ipairs(headline_path) do
-          local headingnr = get_headingnr(i)
-          query = query .. "(heading" .. headingnr .. " title: (paragraph_segment) @t" .. i .. " (#eq? @t" .. i .. " \"" .. headline .. "\"" .. ") "
-          end_parens = end_parens .. ")"
-        end
-        query = query ..  " ) @org-capture-target" .. end_parens
-        return query
-      end
-
-      local exec_query = function(query)
-        ts.execute_query(query, cb, target_bufnr)
-      end
-
-      local build_and_execute_query = function(path_or_headline)
-        local query = build_query(path_or_headline)
-        exec_query(query)
-      end
-
-      if data.headline then
-        build_and_execute_query({ data.headline })
-      elseif data.path then
-        build_and_execute_query(data.path)
-      elseif data.query then
-        exec_query(data.query)
+    local end_line = function(node)
+      local child_count = node:child_count()
+      if child_count > 0 then
+        local child = node:child(child_count - 1)
+        return child:end_()
       else
-        set_lines_and_write(-1) -- Negative one means the end of the file
+        return node:end_()
       end
     end
 
+    local set_lines_and_write = function(end_linenr)
+      vim.api.nvim_buf_set_lines(target_bufnr, end_linenr, end_linenr, false, lines)
+      vim.api.nvim_buf_call(target_bufnr, function()
+
+        --Works if the buffer was already open
+        vim.cmd.norm( { range = { end_linenr + 1, end_linenr + #lines }, args = { "==" } })
+
+        vim.cmd.write({ args = { path }, bang = true })
+      end)
+    end
+
+    local cb = function(query, id, node, _)
+      if(query.captures[id] ~= "org-capture-target") then
+        return false
+      end
+      local end_linenr = end_line(node)
+      set_lines_and_write(end_linenr)
+      return true -- Returning true makes `ts.execute_query` stop iterating over captures
+    end
+
+    local get_headingnr = function(i)
+      if i > 6 then
+        return 6
+      else
+        return i
+      end
+    end
+
+    local build_query = function(headline_path)
+      local query = "("
+      local end_parens = ""
+      for i, headline in ipairs(headline_path) do
+        local headingnr = get_headingnr(i)
+        query = query .. "(heading" .. headingnr .. " title: (paragraph_segment) @t" .. i .. " (#eq? @t" .. i .. " \"" .. headline .. "\"" .. ") "
+        end_parens = end_parens .. ")"
+      end
+      query = query ..  " ) @org-capture-target" .. end_parens
+      return query
+    end
+
+    local exec_query = function(query)
+      ts.execute_query(query, cb, target_bufnr)
+    end
+
+    local build_and_execute_query = function(path_or_headline)
+      local query = build_query(path_or_headline)
+      exec_query(query)
+    end
+
+    if data.headline then
+      build_and_execute_query({ data.headline })
+    elseif data.path then
+      build_and_execute_query(data.path)
+    elseif data.query then
+      exec_query(data.query)
+    else
+      set_lines_and_write(-1) -- Negative one means the end of the file
+    end
   end
+
 }
 
 module.on_event = function(event)
@@ -239,7 +235,6 @@ module.on_event = function(event)
         data = {
           template = data[idx].template,
           file = data[idx].file,
-          type = data[idx].type,
           headline = data[idx].headline,
           path = data[idx].path,
           query = data[idx].query,
